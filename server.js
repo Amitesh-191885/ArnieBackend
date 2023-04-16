20ac3c907026e2878dc713d9c77842774c2e1e73
Amitesh Ranjan <amiteshranjan600@gmail.com>
2023-04-16 09:43:36 +0530

    First Change

---
+++ server.js
@@ -0,0 +1,111 @@
+// const app = require("./app");
+// const cloudinary = require("cloudinary");
+// const connectDatabase = require("./config/database");
+
+// // Handling Uncaught Exception
+// process.on("uncaughtException", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Uncaught Exception`);
+//   process.exit(1);
+// });
+
+// // Config
+// if (process.env.NODE_ENV !== "PRODUCTION") {
+//   require("dotenv").config({ path: "backend/config/config.env" });
+// }
+
+// // Connecting to database
+// connectDatabase();
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const server = app.listen(process.env.PORT, () => {
+//   console.log(`Server is working on http://localhost:${process.env.PORT}`);
+// });
+
+// // Unhandled Promise Rejection
+// process.on("unhandledRejection", (err) => {
+//   console.log(`Error: ${err.message}`);
+//   console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+// second file
+
+require('dotenv').config();
+
+const express = require('express')
+const mongoose = require('mongoose')
+const Book = require("./models/books");
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+//Routes go here
+app.get('/', (req,res) => {
+    res.send({ title: 'Books' });
+})
+
+app.get('/books', async (req,res)=> {
+
+  const book = await Book.find();
+
+  if (book) {
+    res.json(book)
+  } else {
+    res.send("Something went wrong.");
+  }
+  
+});
+
+app.get('/add-note', async (req,res) => {
+  try {
+    await Book.insertMany([
+      {
+        title: "Sons Of Anarchy",
+        body: "Body text goes here...",
+      },
+      {
+        title: "Games of Thrones",
+        body: "Body text goes here...",
+      }
+    ]);
+    res.json({"Data":"Added"})
+  } catch (error) {
+    console.log("err", + error);
+  }
+})
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
